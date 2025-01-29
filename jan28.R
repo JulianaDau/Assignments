d6fd2f0f5911ae1e0658a9e0a421e065d2c23f69
JulianaDau <Juliana.Barbosa-Torreao-Dau@uvm.edu>
2025-01-29 15:56:33 -0500

    ..

---
+++ jan28.R
@@ -0,0 +1,21 @@
+#Introduction to R script
+#January 28
+#Juliana
+-
+# Using the assignment operator
+x <- 5 # preferred
+y = 4 # legal but not used except in function defaults
+y = y + 1.1
+print(y)
+y <- y + 1.1
+print(y)
+-
+z <- 3 # Begin with lower case letter 
+plantHeight <- 10 # option "camelCaseFormatting"
+plant.height <- 4.2 # avoid periods
+plant_height <- 3.3  # optimal "snake_case_formatting"
+. <- 5.5 # reserve this for a generic temporary variable (more later)
+
+
+
+
