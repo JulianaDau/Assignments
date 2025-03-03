1337327db080b2bb4a1156ad2dbf455d072677d1
JulianaDau <Juliana.Barbosa-Torreao-Dau@uvm.edu>
2025-03-02 19:24:11 -0500

    Homework7

--- homework7.R
+++ homework7.R
@@ -15,4 +15,18 @@ iris2<-select(iris1, Sepal.Length, Sepal.Width, Species)
 iris3<-arrange(iris2, by = desc(Sepal.Length))
 print(iris3)
 head(iris3)
-select(iris3,rows=6)
+
+iris4<-mutate(iris3, ratio = Sepal.Length*Sepal.Width)
+head(iris4)
+
+iris5<-summarize(iris4, Sepal_average_Length = mean(Sepal.Length), Sepal_average_Width = mean(Sepal.Width),Sample_Size = n())
+print(iris5)
+iris6<-group_by(iris, Species)
+summarize(iris4, Sepal_average_Length = mean(Sepal.Length), Sepal_average_Width = mean(Sepal.Width),Sample_Size = n())
+print(iris6)
+
+
+irisFinal<-filter(iris,Species != "setosa", Sepal.Length > 6, Sepal.Width > 2.5) %>%
+  group_by(Species) %>%
+  summarize(Sepal_average_Length = mean(Sepal.Length), Sepal_average_Width = mean(Sepal.Width),Sample_Size = n()) 
+print(irisFinal)
